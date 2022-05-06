628a294c8ba6296ed789225becfddccb3825225c
Ruban Sahoo <rubansahoo1@gmail.com>
2022-05-06 14:27:05 +0530

    day 22

---
+++ Medium/069 Task Scheduler/main.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> v(26, 0);
+        int len = tasks.size();
+        for(int i=0; i<len; i++) v[tasks[i] - 'A']++;
+        sort(v.begin(), v.end());
+        int minlen = (n + 1) * (v[25] - 1) + 1;
+        for(int i=24; i>=0; i--) if(v[i] == v[25]) minlen++;
+        return max(minlen, len);
+    }
+};
